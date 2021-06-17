2d8a71a5c3a2973a3682b6b1096eee462ce1ea22
ThePicoNerd <ake@amcoff.net>
2021-06-17 12:46:41 +0200

    Add Chromatic CI

--- src/components/menu/tile.tsx
+++ src/components/menu/tile.tsx
@@ -12,7 +12,7 @@ export interface MenuTileProps {
  * A tile displaying information about a menu.
  *
  * @todo Does this work with SSR?
- * 
+ *
  * @param {React.PropsWithChildren<MenuTileProps>} props Props.
  * @returns {React.ReactElement} A rendered menu tile.
  */
@@ -22,12 +22,12 @@ const MenuTile: FunctionComponent<MenuTileProps> = ({ menu }) => {
 
   return (
     <Link href={`/menus/${menu.id}`}>
-      <a className="bg-white rounded-lg shadow-sm hover:shadow-lg transition-shadow p-4 my-2 block">
+      <a className="bg-white rounded-xl shadow-sm hover:shadow-lg transition-shadow p-4 my-2 block">
         <div className="font-medium">
           <h3 className="text-gray-400">{menu.provider.name}</h3>
           <h2>{menu.title}</h2>
-          <MenuTicker query={{ menu: menu.id, first, last, }} />
         </div>
+        <MenuTicker query={{ menu: menu.id, first, last }} />
       </a>
     </Link>
   );
