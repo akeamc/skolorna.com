659c7964ab6e9fe68325b76774d01f93fb2b52ff
ThePicoNerd <ake@amcoff.net>
2021-06-17 12:35:28 +0200

    fix storybook

--- src/components/menu/tile.tsx
+++ src/components/menu/tile.tsx
@@ -1,3 +1,4 @@
+import { DateTime } from "luxon";
 import Link from "next/link";
 import React, { FunctionComponent } from "react";
 import { Menu } from "../../lib/menu-proxy/types";
@@ -7,14 +8,25 @@ export interface MenuTileProps {
   menu: Menu;
 }
 
+/**
+ * A tile displaying information about a menu.
+ *
+ * @todo Does this work with SSR?
+ * 
+ * @param {React.PropsWithChildren<MenuTileProps>} props Props.
+ * @returns {React.ReactElement} A rendered menu tile.
+ */
 const MenuTile: FunctionComponent<MenuTileProps> = ({ menu }) => {
+  const first = DateTime.now();
+  const last = first.plus({ weeks: 1 });
+
   return (
     <Link href={`/menus/${menu.id}`}>
       <a className="bg-white rounded-lg shadow-sm hover:shadow-lg transition-shadow p-4 my-2 block">
         <div className="font-medium">
           <h3 className="text-gray-400">{menu.provider.name}</h3>
           <h2>{menu.title}</h2>
-          <MenuTicker query={{ menu: menu.id }} />
+          <MenuTicker query={{ menu: menu.id, first, last, }} />
         </div>
       </a>
     </Link>
