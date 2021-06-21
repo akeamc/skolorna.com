4ceb623d78abb83d740ceb5e2eddd5780d2d13c0
ThePicoNerd <ake@amcoff.net>
2021-06-21 12:11:13 +0200

    add homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <p>helo</p>;
+  return (
+    <div>
+      <h1>Skolorna</h1>
+      <style jsx>{`
+        h1 {
+          font-size: 20vw;
+          font-weight: 700;
+          letter-spacing: -0.022em;
+          user-select: none;
+        }
+
+        div {
+          min-height: 100vh;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+      `}</style>
+      <style global jsx>{`
+        body {
+          background: black;
+          color: white;
+        }
+      `}</style>
+    </div>
+  );
 };
 
 export default Home;
