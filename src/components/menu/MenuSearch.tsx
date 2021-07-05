40978c311ce2e51d3d63a8cbf7337b9cacec0bc9
ThePicoNerd <ake@amcoff.net>
2021-07-05 16:00:21 +0200

    remove excess skeleton

--- src/components/menu/MenuSearch.tsx
+++ src/components/menu/MenuSearch.tsx
@@ -5,6 +5,55 @@ import { useMenuSearch } from "../../lib/menu-proxy/menu";
 import MenuTile from "./MenuTile";
 import styles from "./MenuSearch.module.scss";
 import InlineSkeleton from "../skeleton/InlineSkeleton";
+import { Menu } from "../../lib/menu-proxy/types";
+
+interface SearchResultsProps {
+  results: Menu[];
+  searching: boolean;
+  initializing: boolean;
+  query: string;
+  size: number;
+}
+
+const SearchResults: FunctionComponent<SearchResultsProps> = ({
+  results,
+  searching,
+  query,
+  initializing,
+  size,
+}) => {
+  const noResults = !searching && results.length === 0;
+  const gridSkeleton = initializing || (searching && results.length === 0);
+
+  if (noResults) {
+    return (
+      <span>
+        Inga menyer som matchar &quot;<strong>{query}</strong>&quot; hittades.
+      </span>
+    );
+  }
+
+  return (
+    <>
+      <div className={styles.count}>
+        {initializing ? (
+          <InlineSkeleton width="6em" />
+        ) : (
+          <>
+            {results.length} av {size}
+          </>
+        )}
+      </div>
+      <div className={styles.grid}>
+        {(gridSkeleton ? new Array(12).fill(undefined) : results).map(
+          (menu, i) => (
+            <MenuTile menu={menu} key={menu?.id ?? i} />
+          )
+        )}
+      </div>
+    </>
+  );
+};
 
 const MenuSearch: FunctionComponent = () => {
   const limit = 40;
@@ -18,7 +67,6 @@ const MenuSearch: FunctionComponent = () => {
   }, [setQuery, value]);
 
   const initializing = size === 0;
-  const noResults = !searching && results.length <= 0;
 
   return (
     <div>
@@ -35,34 +83,9 @@ const MenuSearch: FunctionComponent = () => {
           disabled={initializing}
         />
       </div>
+
       <section className={styles.results}>
-        {noResults ? (
-          <span>
-            Inga menyer som matchar &quot;<strong>{query}</strong>&quot;
-            hittades.
-          </span>
-        ) : (
-          <>
-            <div className={styles.count}>
-              {initializing ? (
-                <InlineSkeleton width="6em" />
-              ) : (
-                <>
-                  {searching ? <InlineSkeleton width="1em" /> : results.length}{" "}
-                  av {size}
-                </>
-              )}
-            </div>
-            <div className={styles.grid}>
-              {(initializing || searching
-                ? new Array(12).fill(null)
-                : results
-              ).map((menu, index) => (
-                <MenuTile menu={menu} key={menu?.id ?? index} />
-              ))}
-            </div>
-          </>
-        )}
+        <SearchResults {...{ searching, initializing, results, query, size }} />
       </section>
     </div>
   );
