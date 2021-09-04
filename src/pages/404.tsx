dc1865b876e8e2e2238fd5d063f9d923633fbfb6
ThePicoNerd <ake@amcoff.net>
2021-09-04 16:42:29 +0200

    Update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,36 +1,7 @@
 import React from "react";
 import { NextPage } from "next";
-import Link from "next/link";
-import Seo from "../components/Seo";
-import ArrowLink from "../components/link/ArrowLink";
-import PageHeading from "../components/typography/PageHeading";
+import Matrix from "../components/canvas/Matrix";
 
-const NotFound: NextPage = () => (
-  <div className="root">
-    <Seo title="Sidan finns inte" />
-    <div className="text">
-      <PageHeading style={{ marginTop: 0 }}>Sidan finns inte.</PageHeading>
-      <Link href="/" passHref>
-        <ArrowLink>Återvänd</ArrowLink>
-      </Link>
-    </div>
-    <style jsx>{`
-      .root {
-        min-height: 100vh;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        overflow: hidden;
-        padding: var(--page-gutter);
-        box-sizing: border-box;
-      }
-
-      .text {
-        max-width: 768px;
-        text-align: center;
-      }
-    `}</style>
-  </div>
-);
+const NotFound: NextPage = () => <Matrix target="HTTP 404 <<<" />;
 
 export default NotFound;
