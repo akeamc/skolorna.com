1b6e2c79e29f64503eb8f803ba312432ab3ee538
ThePicoNerd <ake@amcoff.net>
2021-09-21 20:26:49 +0200

    :/

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,35 +1,44 @@
-
 import React, { FunctionComponent, useEffect } from "react";
 import { Formik, Field, Form } from "formik";
-import {decode} from "jsonwebtoken";
-import { getAccessToken, login, LoginUser } from "../../lib/auth/api";
+import { decode } from "jsonwebtoken";
+import { refreshAccessToken, login, LoginUser } from "../../lib/auth/api";
+import { useAuth } from "../../lib/auth/context";
 
 const Login: FunctionComponent = () => {
-	useEffect(() => {
-		async function bruh() {
-		const token = await getAccessToken("b9bec1a5-aa6a-42bf-8dc4-3efc89a5d3a9.DXFm6jQGy0mdG5D372ZqOaKY8WQANrSo0DNCrEBDsID7yqmgGvOTUJ0w_nI");
-		const decoded = decode(token);
-		console.log(decoded);
-		console.log(new Date(decoded.exp * 1000));
-		}
+  const { setRefreshToken, setAccessToken } = useAuth();
 
-		bruh();
-	}, []);
+  useEffect(() => {
+    async function bruh() {
+      const token = await refreshAccessToken("");
+      const decoded = decode(token);
+      console.log(decoded);
+      console.log(new Date(decoded.exp * 1000));
+    }
 
-	const initialValues: LoginUser = {
-		email: "",
-		password: "",
-	};
-	
-	return (
-		<Formik onSubmit={login} initialValues={initialValues}>
-			<Form>
-				<Field name="email" type="email" /> 
-				<Field name="password" type="password" />
-				<button type="submit">Submit</button>
-			</Form>
-		</Formik>
-	);
-	};
+    bruh();
+  }, []);
+
+  const initialValues: LoginUser = {
+    email: "",
+    password: "",
+  };
+
+  return (
+    <Formik
+      onSubmit={async (values) => {
+        const { refresh_token, access_token } = await login(values);
+        setRefreshToken(refresh_token);
+        setAccessToken(access_token);
+      }}
+      initialValues={initialValues}
+    >
+      <Form>
+        <Field name="email" type="email" />
+        <Field name="password" type="password" />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  );
+};
 
 export default Login;
