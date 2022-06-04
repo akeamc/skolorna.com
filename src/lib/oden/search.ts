33391356feb1f26b09b31e563ea42dfcec10def5
ThePicoNerd <ake@amcoff.net>
2022-06-04 22:55:34 +0200

    switch to staging temporarily

--- src/lib/oden/search.ts
+++ src/lib/oden/search.ts
@@ -1,8 +1,8 @@
 import { MeiliSearch } from "meilisearch";
 
-export const HOST_URL = "https://api.skolorna.com/v0/search";
+export const HOST_URL = "https://api-staging.skolorna.com/v0/search";
 export const API_KEY =
-  "ab455b91fcf3dc98a68f5005b82b9445f268c4f21cf6a79ab7743d2262dcf040";
+  "Bx550HX6f1da372c436f80be564bfdea7ce136cf98576766c23580f0a92114ad0aecd558";
 export const INDEX_NAME = "menus";
 
 export const searchClient = new MeiliSearch({
