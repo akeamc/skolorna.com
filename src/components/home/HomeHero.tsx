20d098300c18bb55465b32e8eb35dd4eb3c386ef
akeamc <ake@amcoff.net>
2022-07-15 20:12:52 +0200

    :)

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -1,21 +1,54 @@
-import { useFBX, useGLTF } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import React, { FunctionComponent, Suspense } from "react";
+import { PresentationControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import React, {
+  FunctionComponent,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { Group, Object3D } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { MenuStats } from "../../lib/oden/stats";
 import Container from "../layout/Container";
 import styles from "./HomeHero.module.scss";
 
-const Scene: FunctionComponent = () => {
-  const fbx = useGLTF("/plate.glb");
+const Plate: FunctionComponent = () => {
+  const [model, setModel] = useState<Object3D | null>(null);
+  const group = useRef<Group>(null);
+
+  useFrame(() =>
+    group.current ? (group.current.rotation.x += 0.01) : undefined
+  );
+
+  useEffect(() => {
+    const loader = new GLTFLoader();
+    loader.load("/plate.glb", async (gltf) => {
+      const node = await gltf.parser.loadNode(0);
+
+      setModel(node);
+    });
+  }, []);
+
+  if (!model) return null;
 
   return (
+    <group ref={group} scale={20}>
+      <primitive object={model} />
+    </group>
+  );
+};
+
+const Scene: FunctionComponent = () => {
+  return (
     <Canvas>
-      <Suspense fallback={null}>
-      <primitive object={fbx} />
-      </Suspense>
+      <PresentationControls global>
+        <directionalLight />
+        <ambientLight />
+        <Plate />
+      </PresentationControls>
     </Canvas>
-  )
-}
+  );
+};
 
 export const HomeHero: FunctionComponent<{ stats: MenuStats }> = ({
   stats,
@@ -27,8 +60,8 @@ export const HomeHero: FunctionComponent<{ stats: MenuStats }> = ({
       </h1>
       <p className={styles.description}>
         Med sofistikerade algoritmer analyserar vi skolmaten i{" "}
-        {stats.menus.toLocaleString()} skolor. Hittills har vi samlat
-        in {stats.days.toLocaleString()} luncher. 
+        {stats.menus.toLocaleString("sv")} skolor. Hittills har vi samlat in{" "}
+        {stats.days.toLocaleString("sv")} luncher.
       </p>
       <p className={styles.note}>
         <sup>*</sup>Förutom när vi inte vet.
