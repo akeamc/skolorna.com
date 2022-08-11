2af20830b7ee9605ca6755112d516347fd43fa7f
akeamc <ake@amcoff.net>
2022-08-11 11:48:38 +0200

    remove 3d scene

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -1,49 +1,7 @@
-import { PresentationControls } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
-import React, { FunctionComponent, useEffect, useRef, useState } from "react";
-import { Group, Object3D } from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import React, { FunctionComponent } from "react";
 import { MenuStats } from "../../lib/oden/stats";
 import Container from "../layout/Container";
 import styles from "./HomeHero.module.scss";
-
-const Plate: FunctionComponent = () => {
-  const [model, setModel] = useState<Object3D | null>(null);
-  const group = useRef<Group>(null);
-
-  useFrame(() =>
-    group.current ? (group.current.rotation.x += 0.01) : undefined
-  );
-
-  useEffect(() => {
-    const loader = new GLTFLoader();
-    loader.load("/plate.glb", async (gltf) => {
-      const node = await gltf.parser.loadNode(0);
-
-      setModel(node);
-    });
-  }, []);
-
-  if (!model) return null;
-
-  return (
-    <group ref={group} scale={20}>
-      <primitive object={model} />
-    </group>
-  );
-};
-
-const Scene: FunctionComponent = () => {
-  return (
-    <Canvas>
-      <PresentationControls global>
-        <directionalLight />
-        <ambientLight />
-        <Plate />
-      </PresentationControls>
-    </Canvas>
-  );
-};
 
 export const HomeHero: FunctionComponent<{ stats: MenuStats }> = ({
   stats,
@@ -62,6 +20,5 @@ export const HomeHero: FunctionComponent<{ stats: MenuStats }> = ({
         <sup>*</sup>Förutom när vi inte vet.
       </p>
     </div>
-    <Scene />
   </Container>
 );
