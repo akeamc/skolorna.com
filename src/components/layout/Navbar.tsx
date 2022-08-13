3b76bcbaa99d7c5cf206b5d137815e50a3cff674
akeamc <ake@amcoff.net>
2022-08-14 01:26:14 +0200

    navbar (mobile is wip)

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,15 +2,45 @@ import React, { FunctionComponent } from "react";
 import Container from "./Container";
 import styles from "./Navbar.module.scss";
 import Search from "../menu/Search";
+import { LogoIcon } from "../brand/Icon";
+import Link from "next/link";
+
+interface Item {
+  label: string;
+  href: string;
+}
+
+const items: Item[] = [
+  {
+    label: "Menyer",
+    href: "/menyer",
+  },
+  {
+    label: "Blogg",
+    href: "/blogg",
+  },
+];
 
 export const Navbar: FunctionComponent = () => {
   return (
-    <nav className={styles.nav}>
-      <Container>
-        <div className={styles.horizontal}>
-          <Search />
-        </div>
-      </Container>
-    </nav>
+    <Container>
+      <div className={styles.wrapper}>
+        <Link href="/">
+          <a className={styles.logo}>
+            <LogoIcon />
+          </a>
+        </Link>
+        <Search />
+        <nav className={styles.nav}>
+          <ul>
+            {items.map(({ label, href }) => (
+              <li key={href}>
+                <a href={href}>{label}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </Container>
   );
 };
