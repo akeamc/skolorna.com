7badce2a103d5ec420825896daa8a1cb2f290350
akeamc <ake@amcoff.net>
2023-03-04 21:41:20 +0100

    don't show login prompt if authenticating

--- src/lib/auth/GoogleId.svelte
+++ src/lib/auth/GoogleId.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from "svelte";
-	import { authenticate, authenticated } from "./auth";
+	import { authenticate, authenticated, authenticating } from "./auth";
 
 	export let clientId: string;
 
@@ -30,7 +30,7 @@
 		initialized = true;
 	});
 
-	$: if (initialized && !$authenticated) {
+	$: if (initialized && !$authenticated && !$authenticating) {
 		google.accounts.id.prompt();
 	}
 </script>
