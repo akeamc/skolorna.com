06fbc5ed4f7d546bf2ec9d46f5a9f8a1d6a47897
akeamc <ake@amcoff.net>
2023-08-29 14:03:03 +0200

    fix padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -83,7 +83,7 @@ export default function Header() {
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-4 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 md:px-6">
           <div className="flex items-center justify-between">
             <LogoLink />
 
